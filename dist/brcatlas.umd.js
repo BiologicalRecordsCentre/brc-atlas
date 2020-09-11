4e62979d77be39bf70cbaae149c11d202ad89d59
Rich Burkmar <rich.burkmar@gmail.com>
2020-09-11 21:00:51 +0100

    Bumped version to 0.1.1

--- dist/brcatlas.umd.js
+++ dist/brcatlas.umd.js
@@ -9203,7 +9203,7 @@
   }
 
   var name = "brcatlas";
-  var version = "0.1.0";
+  var version = "0.1.1";
   var description = "Javascript library for web-based biological records atlas mapping in the British Isles.";
   var type = "module";
   var main = "dist/brcatlas.umd.js";
