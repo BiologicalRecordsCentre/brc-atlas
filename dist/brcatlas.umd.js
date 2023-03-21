7523d2472ac457fdaa65aa776202e566b1f4ac8f
burkmarr <rich.burkmar@gmail.com>
2023-03-21 07:42:46 -0700

    Bumping minor version number

--- dist/brcatlas.umd.js
+++ dist/brcatlas.umd.js
@@ -506,7 +506,7 @@
   };
 
   var name = "brcatlas";
-  var version = "1.0.1";
+  var version = "1.0.2";
   var description = "Javascript library for web-based biological records atlas mapping in the British Isles.";
   var type = "module";
   var main = "dist/brcatlas.umd.js";
